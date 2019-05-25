a3ab226f1b8b3084d6d8097794184d18226c1ca3
DGWerlod <ddeanda333@gmail.com>
2019-05-25 14:39:55 -0700

    Added processing files
    Signed-off-by: DGWerlod <ddeanda333@gmail.com>

---
+++ src/DrawingSurface.java
@@ -0,0 +1,27 @@
+package dgwerlod.mazes;
+
+import processing.core.PApplet;
+
+public class DrawingSurface extends PApplet {
+
+    public DrawingSurface() {
+
+    }
+
+    // The statements in the setup() function
+    // execute once when the program begins
+    public void setup() {
+
+    }
+
+    // The statements in draw() are executed until the
+    // program is stopped. Each statement is executed in
+    // sequence and after the last line is read, the first
+    // line is executed again.
+    public void draw() {
+
+        background(255);   // Clear the screen with a white background
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        dgwerlod.mazes.DrawingSurface drawing = new dgwerlod.mazes.DrawingSurface();
+        PApplet.runSketch(new String[]{""}, drawing);
+        PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
+        PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+        JFrame window = (JFrame)canvas.getFrame();
+
+        // 7, 30 used to make surface itself perfectly square
+        window.setSize(500 + 7, 500 + 30);
+        window.setMinimumSize(new Dimension(100,100));
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(true);
+
+        window.setVisible(true);
+        canvas.requestFocus();
     }
 
 }
