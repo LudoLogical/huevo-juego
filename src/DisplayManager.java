3ae95c2508e66cf166700c88f9c00b955a062827
DGWerlod <ddeanda333@gmail.com>
2019-05-26 01:17:48 -0700

    Added screen transitions
    Signed-off-by: DGWerlod <ddeanda333@gmail.com>

--- src/DisplayManager.java
+++ src/DisplayManager.java
@@ -31,6 +31,7 @@ public class DisplayManager extends PApplet {
     // line is executed again.
     public void draw() {
         background(255);   // Clear the screen with a white background
+        handleTypingBar();
         if (activeDisplay == 0) {
             eDisplay.draw(this);
         } else if (activeDisplay == 1) {
@@ -40,7 +41,20 @@ public class DisplayManager extends PApplet {
         } else {
             throw new IllegalArgumentException("Invalid activeDisplay value: " + activeDisplay);
         }
-        handleTypingBar();
+        stroke(0);
+        fill(255);
+        if (activeDisplay == 0 || activeDisplay == 2) {
+            rect(0, height/2 - 25, 50, 50);
+            line(10, height/2, 40, height/2);
+            line(10, height/2, 15, height/2 - 5);
+            line(10, height/2, 15, height/2 + 5);
+        }
+        if (activeDisplay == 0 || activeDisplay == 1) {
+            rect(width - 50, height/2 - 25, 50, 50);
+            line(width - 10, height/2, width - 40, height/2);
+            line(width - 10, height/2, width - 15, height/2 - 5);
+            line(width - 10, height/2, width - 15, height/2 + 5);
+        }
     }
 
     private void handleTypingBar() {
@@ -53,6 +67,24 @@ public class DisplayManager extends PApplet {
         if (addHuevos) {
             huevoManager.addTypedHuevos();
             eDisplay.addfallingegg();
+        }
+    }
+
+    public void mousePressed() {
+        boolean leftRegion = mouseX > 0 && mouseX < 50 && mouseY > width/2 - 25 && mouseY < width/2 + 25;
+        boolean rightRegion = mouseX > width-50 && mouseX < width && mouseY > width/2 - 25 && mouseY < width/2 + 25;
+        if (leftRegion) {
+            if (activeDisplay == 0) {
+                activeDisplay = 1;
+            } else if (activeDisplay == 2) {
+                activeDisplay = 0;
+            }
+        } else if (rightRegion) {
+            if (activeDisplay == 0) {
+                activeDisplay = 2;
+            } else if (activeDisplay == 1) {
+                activeDisplay = 0;
+            }
         }
     }
 
